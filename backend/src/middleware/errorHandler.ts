9c2100c4f272f7058c07ca2aa32cc6cb89c19663
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-03 19:49:31 -0700

    Return errors for validation

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,14 +1,19 @@
 import { NextFunction, Response, Request } from "express"
 import { IErrorWithStatusCode } from "../classes/ErrorWithStatusCode"
+import { IValidationError, ValidationError } from "../classes/ValidationError"
 
 export const errorHandler = (
-	err: IErrorWithStatusCode,
+	err: IErrorWithStatusCode | IValidationError<any>,
 	req: Request,
 	res: Response,
-    next: NextFunction //
+	next: NextFunction // REQUIRED
 ) => {
 	const statusCode = err.statusCode ?? 500
 	const message = !err.statusCode ? "Internal Server Error" : err.message
+
+	if (err instanceof ValidationError) {
+		return res.status(statusCode).json({ statusCode, message, errors: err.errors.issues })
+	}
 
 	return res.status(statusCode).json({ statusCode, message })
 }
