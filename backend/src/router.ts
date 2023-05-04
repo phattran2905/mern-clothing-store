04a3812215209706b8114b9413878fa48ef1d049
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-03 19:50:31 -0700

    Add sign-up route

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 import authRouter from './authentication/auth.router';
+import signUpRouter from './signUp/signUp.router';
 
 const router = Router()
 
 // Routes
 authRouter(router)
+signUpRouter(router)
 
 export default router
 
