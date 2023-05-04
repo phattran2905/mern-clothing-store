5c7e9a240200652f97f2c0b2c79727a60832d5f7
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-03 19:59:51 -0700

    Change login route's method to POST

--- backend/src/authentication/auth.router.ts
+++ backend/src/authentication/auth.router.ts
@@ -2,7 +2,7 @@ import { Response, Router, Request } from "express"
 import { login } from "./auth.controller"
 
 const authRouter = (router: Router) => {
-	router.get("/login", login)
+	router.post("/login", login)
 }
 
 export default authRouter
