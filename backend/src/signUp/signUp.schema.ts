868970c8098086c8527bbb57e9d22c513d9ed9fb
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-03 19:50:05 -0700

    Add validation for signUpForm

--- backend/src/signUp/signUp.schema.ts
+++ backend/src/signUp/signUp.schema.ts
@@ -1,9 +1,34 @@
 import z from "zod"
+import { findUserByEmail } from "../authentication/auth.model"
 
-export const SignUpSchema = z.object({
-	email: z.string(),
-	password: z.string(),
-	confirm_password: z.string(),
-})
+const SignUpForm = z
+	.object({
+		email: z.string().email({ message: "Invalid email address" }),
+		password: z.string().min(6),
+		confirm: z.string().min(6),
+	})
+	.superRefine(async (data, ctx) => {
+		if (data.password !== data.confirm) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "Password don't match",
+                path: ["password", "confirm"],
+                fatal: true,
+			})
 
-type SignUpSchema = z.infer<typeof SignUpSchema>
+            return z.NEVER;
+		}
+
+        const existingEmail = await findUserByEmail(data.email)
+		if (existingEmail) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: `Email already exists.`,
+                path: ["email"],
+			})
+		}
+	})
+
+type SignUpForm = z.infer<typeof SignUpForm>
+
+export default SignUpForm
--- backend/src/signUp/signUp.types.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod"
-
-const SignUpSchema = z.object({
-	email: z.string(),
-	password: z.string(),
-	confirm_password: z.string(),
-})
-
-export type SignUpSchema = z.infer<typeof SignUpSchema>
