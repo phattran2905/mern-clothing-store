95c8d5b48013b4e0c37418ddf3bdb75d706c799e
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-05 18:21:48 -0700

    Add API for removing account by ID

--- backend/src/account/account.controller.ts
+++ backend/src/account/account.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express"
-import { findAccountById, findAllAccounts } from "./account.model"
+import { deleteAccountById, findAccountById, findAllAccounts } from "./account.model"
 import { ErrorWithStatusCode } from "../classes/ErrorWithStatusCode"
 
 export const getAllAccounts = async (req: Request, res: Response, next: NextFunction) => {
@@ -27,3 +27,22 @@ export const getAccountById = async (req: Request, res: Response, next: NextFunc
 	}
 }
 
+export const removeAccountById = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+        const currentAdmin = res.locals.user
+		const accountId = req.params.id ?? null
+
+        if (currentAdmin.id === accountId) {
+            throw new ErrorWithStatusCode(400, `Can not self delete your logging account.`)
+        }
+
+		const account = await deleteAccountById(accountId)
+		if (!account) {
+			throw new ErrorWithStatusCode(500, `Failed to delete the account with id ${accountId}`)
+		}
+
+		return res.status(200).json({ statusCode: 200, message: "Deleted." })
+	} catch (error) {
+		return next(error)
+	}
+}
--- backend/src/account/account.router.ts
+++ backend/src/account/account.router.ts
@@ -1,13 +1,13 @@
-import {Router} from 'express';
-import { validateJWT } from '../middleware/jwtAuthentication';
-import { isAdminRole } from '../middleware/roleChecking';
-import { getAccountById, getAllAccounts } from './account.controller';
-
+import { Router } from "express"
+import { validateJWT } from "../middleware/jwtAuthentication"
+import { isAdminRole } from "../middleware/roleChecking"
+import { getAccountById, getAllAccounts, removeAccountById } from "./account.controller"
 
 const accountRouter = (router: Router) => {
 	router
 		.get("/admin/accounts", validateJWT, isAdminRole, getAllAccounts)
-        .get("/admin/accounts/:id",validateJWT, isAdminRole, getAccountById)
+		.get("/admin/accounts/:id", validateJWT, isAdminRole, getAccountById)
+		.delete("/admin/accounts/:id", validateJWT, isAdminRole, removeAccountById)
 }
 
 export default accountRouter
